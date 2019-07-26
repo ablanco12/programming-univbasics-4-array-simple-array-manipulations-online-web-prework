d440d41471a38995e4133428637fbd3c8b531707
Andrew-Joseph Blanco <aj.blanco@rocketmail.com>
2019-07-26 03:09:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,5 @@
 def using_push(Array, String)
-  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-  next_color = "violet"
-  colors_in_the_rainbow.push(next_color)
+  
 end
 
 def using_unshift(Array, String)
